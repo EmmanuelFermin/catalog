8bbf95948ab142c29576a158ddf1851c1bfb14a5
Emmanuel Fermin <emmanuelfermin1990@gmail.com>
2022-09-06 21:31:37 +0800

    Initial Commit w/ added assets of font files, logo and icons

---
+++ icons/UncheckedBox.tsx
@@ -0,0 +1,27 @@
+import { createSvgIcon } from "@mui/material/utils";
+
+const UncheckedBox = createSvgIcon(
+  <svg
+    version="1.0"
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    viewBox="0 0 1250.000000 1250.000000"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <g
+      transform="translate(0.000000,1250.000000) scale(0.100000,-0.100000)"
+      fill="#000000"
+      stroke="none"
+    >
+      <path
+        d="M0 6250 l0 -6250 6250 0 6250 0 0 6250 0 6250 -6250 0 -6250 0 0
+-6250z m11395 5145 l25 -24 0 -5121 0 -5121 -25 -24 -24 -25 -5121 0 -5121 0
+-24 25 -25 24 0 5121 0 5121 25 24 24 25 5121 0 5121 0 24 -25z"
+      />
+    </g>
+  </svg>,
+  "UncheckedBox"
+);
+
+export default UncheckedBox;
